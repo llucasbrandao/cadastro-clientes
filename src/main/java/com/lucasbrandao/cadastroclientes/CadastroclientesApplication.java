0981f036b95d1f0ecab90cb04329a804c2f68f41
Lucas Brandão <llucasbrandaodev@gmail.com>
2021-02-02 14:12:43 -0300

    initial config

---
+++ src/main/java/com/lucasbrandao/cadastroclientes/CadastroclientesApplication.java
@@ -0,0 +1,13 @@
+package com.lucasbrandao.cadastroclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroclientesApplication.class, args);
+	}
+
+}
